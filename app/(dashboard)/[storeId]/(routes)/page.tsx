b58d01650f82fd988be6a75adaa008fd3a03271b
Kalmovic <joseph.ext@rallyrd.com>
2023-08-24 15:22:45 -0300

    chore: init page for store

---
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -0,0 +1,19 @@
+import prismadb from "@/lib/prismadb";
+import React from "react";
+
+type DashboardPageProps = {
+  params: {
+    storeId: string;
+  };
+};
+
+async function DashboardPage({ params }: DashboardPageProps) {
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+    },
+  });
+  return <div>Active store: {store?.name}</div>;
+}
+
+export default DashboardPage;
