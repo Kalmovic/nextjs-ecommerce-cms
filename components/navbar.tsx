1a6fc1149b8fc662f7fdbb9885bda2a10d748a36
Kalmovic <joseph.ext@rallyrd.com>
2023-08-24 18:28:05 -0300

    feat: new component navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { redirect } from "next/navigation";
+import { UserButton, auth } from "@clerk/nextjs";
+import prismadb from "@/lib/prismadb";
+import MainNav from "@/components/main-nav";
+import StoreSwitcher from "@/components/store-switcher";
+
+async function NavBar() {
+  const { userId } = auth();
+
+  if (!userId) redirect("/sign-in");
+
+  const stores = await prismadb.store.findMany({
+    where: {
+      userId,
+    },
+  });
+
+  return (
+    <nav className="border-b flex">
+      <section className="flex h-16 items-center px-4 w-full">
+        <StoreSwitcher items={stores} />
+        <MainNav className="mx-6" />
+        <section className="ml-auto flex items-center space-x-4">
+          <UserButton afterSignOutUrl="/" />
+        </section>
+      </section>
+    </nav>
+  );
+}
+
+export default NavBar;
